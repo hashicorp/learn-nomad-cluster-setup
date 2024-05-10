b9787435b7d6170b8602e805d9ea3f013c208402
David Yu <dyu@hashicorp.com>
2024-05-10 14:39:28 -0700

    Update setup.sh

--- shared/scripts/setup.sh
+++ shared/scripts/setup.sh
@@ -72,6 +72,7 @@ JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 # Install HashiCorp Apt Repository
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) test" | sudo tee -a /etc/apt/sources.list.d/hashicorp.list
 
 # Install HashiStack Packages
 sudo apt-get update && sudo apt-get -y install \
